af6a57be6d8cd7eb560a859d26ed781dba0156a0
carlositos <stykhonchuk@gmail.com>
2016-11-17 17:47:50 +0200

    clear db config data

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=andrew_test',
-    'username' => 'root',
-    'password' => 'q1w2e3Q@',
+    'dsn' => 'mysql:host=;dbname=',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
 ];
